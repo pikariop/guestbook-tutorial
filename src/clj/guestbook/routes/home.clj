261f2f3d2a1bcd8478cf34a7ed69e0719064d7a1
pikariop <olli.pikarinen@striim.in>
2016-08-17 14:10:04 +0300

    Display and post messages

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,17 +1,25 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [guestbook.db.core :as db]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"
+    {:messages (db/get-messages)}))
 
 (defn about-page []
   (layout/render "about.html"))
 
+(defn save-message! [{:keys [params]}]
+  (db/save-message!
+   (assoc params :timestamp (java.util.Date.)))
+  (response/found "/"))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (POST "/message" request (save-message! request)))
 
