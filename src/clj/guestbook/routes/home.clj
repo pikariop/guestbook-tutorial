f3330c224407827f775c8c0bf715b63afc889c3a
pikariop <olli.pikarinen@striim.in>
2016-08-17 14:35:48 +0300

    Add validation

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -3,23 +3,37 @@
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]
-            [guestbook.db.core :as db]))
+            [guestbook.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]))
 
-(defn home-page []
+(defn validate-message [params]
+  (first
+   (b/validate
+    params
+    :name v/required
+    :message [v/required [v/min-count 10]])))
+
+(defn home-page [{:keys [flash]}]
   (layout/render
     "home.html"
-    {:messages (db/get-messages)}))
+    (merge {:messages (db/get-messages)}
+           (select-keys flash [:name :message :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defn save-message! [{:keys [params]}]
-  (db/save-message!
-   (assoc params :timestamp (java.util.Date.)))
-  (response/found "/"))
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+        (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
   (GET "/about" [] (about-page))
   (POST "/message" request (save-message! request)))
 
